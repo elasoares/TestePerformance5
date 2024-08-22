010de27bcfac318bdd3ded3c58d2ebc8f0a06f6d
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2024-08-22 13:27:18 -0300

    Add files via upload

---
+++ TestePerformance5/src/main/java/Principal.java
@@ -0,0 +1,25 @@
+public class Principal {
+    public static void main(String[] args) {
+        int numero = 5;
+        int fatorial =  calcularFatorial(numero);
+        System.out.println("O fatorial de " + numero + " é: " + fatorial);
+    }
+    public static int calcularFatorial(int numero) {
+
+        if (numero == 0) {
+            return 1;
+        }
+
+        if(numero < 0){
+            throw new ArithmeticException("Erro ao calcular fatorial. Fatorial não pode ser negativo.");
+        }
+
+        int resultado = 1;
+
+        while (numero > 1){
+            resultado  *= numero;
+            numero--;
+        }
+        return resultado;
+    }
+}
